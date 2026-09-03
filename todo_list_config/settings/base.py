777e07f13b9d6f01fac95dba20f2f5254bcb2da2
Pasha Liashenko <zedxi236@ukr.net>
2026-09-03 13:53:23 +0300

    Project Settings

--- todo_list_config/settings/base.py
+++ todo_list_config/settings/base.py
@@ -3,19 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4&!1!f186a^_a!f%gv*00j1w9i@b#sd-0%$ej*z)!1a19rusg-"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -55,18 +42,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "todo_list_config.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

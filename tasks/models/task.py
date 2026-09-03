cf496c538c9f4fbe38f6a20a4d28553faee5f0ec
Pasha Liashenko <zedxi236@ukr.net>
2026-09-03 14:40:14 +0300

    Updated task and tag

--- tasks/models/task.py
+++ tasks/models/task.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+from tasks.models import Tag
+
 
 class Task(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField()
     deadline = models.DateTimeField()
     is_done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.content[:30]

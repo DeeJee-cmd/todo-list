a600fba6069c5edf9655fb0d7588131f415179db
Pasha Liashenko <zedxi236@ukr.net>
2026-09-03 16:23:22 +0300

    feat(tasks): implement Task model, CRUD views, forms and templates

--- tasks/models/task.py
+++ tasks/models/task.py
@@ -5,10 +5,10 @@ from tasks.models import Tag
 
 class Task(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField()
-    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+    tags = models.ManyToManyField("Tag", related_name="tasks", blank=True)
 
     def __str__(self):
         return self.content[:30]
